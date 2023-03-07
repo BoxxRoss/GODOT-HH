73cae602a27b7669ebe7d625cf2fb3f1f2c865e1
William Burns <wsbsc5@yahoo.com>
2023-03-07 15:09:15 -0800

    THE BEAM WORKS YEEEEAAAAAAAAAAAA

--- laser/LaserBeam.gd
+++ laser/LaserBeam.gd
@@ -12,6 +12,8 @@ func _physics_process(delta):
 	if Global.beam_active == false:
 		queue_free()
 	
+	
+	
 	self.position = Global.bullet_pos
 	var mouse_position = get_local_mouse_position()
 	var max_cast_to = mouse_position.normalized() * MAX_LENGTH
@@ -22,13 +24,15 @@ func _physics_process(delta):
 		end.global_position = rayCast2D.cast_to
 	beam.rotation = rayCast2D.cast_to.angle()
 	beam.region_rect.end.x = end.position.length()
-	
+	$Beam/Area2Dbeam/CollisionShape2D.shape.extents = Vector2(end.position.length()/2,10)
+	$Beam/Area2Dbeam/CollisionShape2D.position = Vector2(end.position.length()/2,0)
 
 
-
-func _on_Area2D2_body_entered(body):
-	print("beam hittin")
 
 
 func _on_Area2D_body_entered(body):
 	print("point hitten")
+
+
+func _on_Area2Dbeam_body_entered(body):
+	print("beam hittin")
