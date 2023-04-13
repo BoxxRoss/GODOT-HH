071275a36e8eca8c8f0e747cc0b18e9aff3d4d99
William Burns <wsbsc5@yahoo.com>
2023-04-12 17:52:22 -0700

    it works, still needs tuning on which one it chooses to connect with but thats for future me. hi future me, zoey's sitting next to me

--- Projectile/lightnin/node/eletric_lazer.gd
+++ Projectile/lightnin/node/eletric_lazer.gd
@@ -4,17 +4,12 @@ onready var beam = $Beam
 onready var end = $End
 onready var rayCast2D = $RayCast2D
 
-
-
-
-	
 func _physics_process(delta):
 	if Global.died == true:
 		queue_free()
 	
-	
-
 func _on_electric_node_static_connect(o_spot,spott):
 	self.global_position = spott
 	end.global_position = o_spot
 	beam.region_rect.end.x = end.position.length()
+	beam.rotation = beam.get_angle_to(o_spot)
